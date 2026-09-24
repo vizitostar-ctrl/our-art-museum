6a997f7df2d25606eea6b51783e1ee6ef11a9fcb
vizitostar-ctrl <vizitostar@gmail.com>
2026-09-24 12:52:04 +0900

    Create data.js

---
+++ app/data.js
@@ -0,0 +1,14 @@
+export const classes = [
+  { id: 1, name: "2학년 1반", flag: null },
+  { id: 2, name: "2학년 2반", flag: null },
+  { id: 3, name: "2학년 3반", flag: null },
+  { id: 4, name: "2학년 4반", flag: null },
+  { id: 5, name: "2학년 5반", flag: null },
+  { id: 6, name: "2학년 6반", flag: null },
+  { id: 7, name: "2학년 7반", flag: null },
+  { id: 8, name: "2학년 8반", flag: null },
+  { id: 9, name: "2학년 9반", flag: null },
+  { id: 10, name: "2학년 10반", flag: null },
+];
+
+export const artworks = [];
